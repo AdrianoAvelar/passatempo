efae6d704e322014f455441ce3ef192764dc4560
Adriano <eama@cin.ufpe.br>
2013-11-06 11:32:37 -0300

    Arquivos relationados ao bando de dados. Handler de SQLite e DAO

--- PassaTempo/db.sql
+++ PassaTempo/db.sql
@@ -6,3 +6,20 @@ CREATE  TABLE "main"."clientes"
  "cep" INTEGER, "tel" INTEGER,
  "sexo" CHAR DEFAULT F,
  "data_cadastro"  DEFAULT CURRENT_TIMESTAMP)
+ 
+ CREATE  TABLE "main"."filmes" 
+ ( "id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL ,
+ "titulo_original" VARCHAR,
+ "titulo_traduzido" VARCHAR,
+ "duracao" INTEGER,
+ "ano" INTEGER,
+ "idioma" INTEGER,
+ "genero" VARCHAR,
+ "classificacao" VARCHAR, 
+ "imagem" VARCHAR, 
+UNIQUE ("titulo_original", "titulo_traduzido", "ano")  ON CONFLICT FAIL
+ );
+
+
+
+
